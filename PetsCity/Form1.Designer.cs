614355c34e6311dcf8e1e747711687ba86c48ecf
unknown <2amod@e041edu.local>
2026-09-16 17:20:01 -0300

    feat: Estilizando e adicionando arredondamento aos panels - WinForms

--- PetsCity/Form1.Designer.cs
+++ PetsCity/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlLaranja2 = new System.Windows.Forms.Panel();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.pnlResu = new System.Windows.Forms.Panel();
+            this.lblResu = new System.Windows.Forms.Label();
+            this.pnlDados = new System.Windows.Forms.Panel();
+            this.lblDados = new System.Windows.Forms.Label();
+            this.pnlMarrom2 = new System.Windows.Forms.Panel();
+            this.pnlLaranja1 = new System.Windows.Forms.Panel();
+            this.pnlCreme1 = new System.Windows.Forms.Panel();
+            this.pnlMarrom1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.pnlLaranja2.SuspendLayout();
+            this.pnlResu.SuspendLayout();
+            this.pnlDados.SuspendLayout();
+            this.pnlLaranja1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-40, 629);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 260);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(345, -5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(745, 243);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pnlLaranja2
+            // 
+            this.pnlLaranja2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(141)))), ((int)(((byte)(24)))));
+            this.pnlLaranja2.Controls.Add(this.pnl);
+            this.pnlLaranja2.Controls.Add(this.pnlMarrom2);
+            this.pnlLaranja2.Location = new System.Drawing.Point(709, 276);
+            this.pnlLaranja2.Name = "pnlLaranja2";
+            this.pnlLaranja2.Size = new System.Drawing.Size(526, 347);
+            this.pnlLaranja2.TabIndex = 4;
+            // 
+            // pnl
+            // 
+            this.pnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(228)))), ((int)(((byte)(198)))));
+            this.pnl.Location = new System.Drawing.Point(17, 30);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(475, 301);
+            this.pnl.TabIndex = 5;
+            // 
+            // pnlResu
+            // 
+            this.pnlResu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(37)))), ((int)(((byte)(6)))));
+            this.pnlResu.Controls.Add(this.lblResu);
+            this.pnlResu.Location = new System.Drawing.Point(700, 265);
+            this.pnlResu.Name = "pnlResu";
+            this.pnlResu.Size = new System.Drawing.Size(209, 55);
+            this.pnlResu.TabIndex = 7;
+            // 
+            // lblResu
+            // 
+            this.lblResu.AutoSize = true;
+            this.lblResu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(228)))), ((int)(((byte)(198)))));
+            this.lblResu.Font = new System.Drawing.Font("Forte", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(22)))), ((int)(((byte)(0)))));
+            this.lblResu.Location = new System.Drawing.Point(12, 11);
+            this.lblResu.Name = "lblResu";
+            this.lblResu.Size = new System.Drawing.Size(184, 38);
+            this.lblResu.TabIndex = 0;
+            this.lblResu.Text = "Resultado";
+            // 
+            // pnlDados
+            // 
+            this.pnlDados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(37)))), ((int)(((byte)(6)))));
+            this.pnlDados.Controls.Add(this.lblDados);
+            this.pnlDados.Location = new System.Drawing.Point(155, 265);
+            this.pnlDados.Name = "pnlDados";
+            this.pnlDados.Size = new System.Drawing.Size(254, 55);
+            this.pnlDados.TabIndex = 8;
+            // 
+            // lblDados
+            // 
+            this.lblDados.AutoSize = true;
+            this.lblDados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(228)))), ((int)(((byte)(198)))));
+            this.lblDados.Font = new System.Drawing.Font("Forte", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDados.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(22)))), ((int)(((byte)(0)))));
+            this.lblDados.Location = new System.Drawing.Point(12, 11);
+            this.lblDados.Name = "lblDados";
+            this.lblDados.Size = new System.Drawing.Size(232, 38);
+            this.lblDados.TabIndex = 0;
+            this.lblDados.Text = "Dados do Pet";
+            // 
+            // pnlMarrom2
+            // 
+            this.pnlMarrom2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(37)))), ((int)(((byte)(6)))));
+            this.pnlMarrom2.Location = new System.Drawing.Point(8, 8);
+            this.pnlMarrom2.Name = "pnlMarrom2";
+            this.pnlMarrom2.Size = new System.Drawing.Size(515, 336);
+            this.pnlMarrom2.TabIndex = 6;
+            // 
+            // pnlLaranja1
+            // 
+            this.pnlLaranja1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(141)))), ((int)(((byte)(24)))));
+            this.pnlLaranja1.Controls.Add(this.pnlCreme1);
+            this.pnlLaranja1.Controls.Add(this.pnlMarrom1);
+            this.pnlLaranja1.Location = new System.Drawing.Point(168, 276);
+            this.pnlLaranja1.Name = "pnlLaranja1";
+            this.pnlLaranja1.Size = new System.Drawing.Size(526, 347);
+            this.pnlLaranja1.TabIndex = 9;
+            // 
+            // pnlCreme1
+            // 
+            this.pnlCreme1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(228)))), ((int)(((byte)(198)))));
+            this.pnlCreme1.Location = new System.Drawing.Point(17, 30);
+            this.pnlCreme1.Name = "pnlCreme1";
+            this.pnlCreme1.Size = new System.Drawing.Size(475, 301);
+            this.pnlCreme1.TabIndex = 5;
+            // 
+            // pnlMarrom1
+            // 
+            this.pnlMarrom1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(37)))), ((int)(((byte)(6)))));
+            this.pnlMarrom1.Location = new System.Drawing.Point(8, 8);
+            this.pnlMarrom1.Name = "pnlMarrom1";
+            this.pnlMarrom1.Size = new System.Drawing.Size(515, 336);
+            this.pnlMarrom1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1414, 871);
+            this.Controls.Add(this.pnlDados);
+            this.Controls.Add(this.pnlLaranja1);
+            this.Controls.Add(this.pnlResu);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnlLaranja2);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(21)))), ((int)(((byte)(0)))));
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.pnlLaranja2.ResumeLayout(false);
+            this.pnlResu.ResumeLayout(false);
+            this.pnlResu.PerformLayout();
+            this.pnlDados.ResumeLayout(false);
+            this.pnlDados.PerformLayout();
+            this.pnlLaranja1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel pnlLaranja2;
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Panel pnlResu;
+        private System.Windows.Forms.Label lblResu;
+        private System.Windows.Forms.Panel pnlDados;
+        private System.Windows.Forms.Label lblDados;
+        private System.Windows.Forms.Panel pnlMarrom2;
+        private System.Windows.Forms.Panel pnlLaranja1;
+        private System.Windows.Forms.Panel pnlCreme1;
+        private System.Windows.Forms.Panel pnlMarrom1;
     }
 }
 
--- PetsCity/Form1.cs
+++ PetsCity/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace PetsCity
 {
@@ -15,6 +16,45 @@ namespace PetsCity
         public Form1()
         {
             InitializeComponent();
+            ArredondarPanel(pnlDados, 35);
+            ArredondarPanel(pnlDados2, 35);
+            ArredondarPanel(pnlDados3, 35);
+            ArredondarPanel(pnlResu, 35);
+            ArredondarPanel(pnlLaranja2, 35);
+            ArredondarPanel(pnl, 35);
+            ArredondarLabel(lblDados, 15);
+            ArredondarLabel(lblResu, 15);
         }
+
+        private void ArredondarPanel(Panel panel, int raio)
+        {
+            GraphicsPath caminho = new GraphicsPath();
+
+            caminho.AddArc(0, 0, raio, raio, 180, 90);
+            caminho.AddArc(panel.Width - raio, 0, raio, raio, 270, 90);
+            caminho.AddArc(panel.Width - raio, panel.Height - raio, raio, raio, 0, 90);
+            caminho.AddArc(0, panel.Height - raio, raio, raio, 90, 90);
+
+            caminho.CloseFigure();
+
+            panel.Region = new Region(caminho);
+        }
+
+        private void ArredondarLabel(Label label, int raio)
+        {
+            GraphicsPath caminho = new GraphicsPath();
+
+            caminho.AddArc(0, 0, raio, raio, 180, 90);
+            caminho.AddArc(label.Width - raio, 0, raio, raio, 270, 90);
+            caminho.AddArc(label.Width - raio, label.Height - raio, raio, raio, 0, 90);
+            caminho.AddArc(0, label.Height - raio, raio, raio, 90, 90);
+
+            caminho.CloseFigure();
+
+            label.Region = new Region(caminho);
+        }
+
     }
+
+
 }
